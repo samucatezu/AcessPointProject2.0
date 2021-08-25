88c4b6f86c0c3c5f31e8ea7b193bea5353ac9252
Samuel Barbosa Almeida <barbosasamuel24@gmail.com>
2021-08-25 14:34:31 -0300

    First commit

---
+++ src/main/java/com/dio/personalProject/LiveApplication.java
@@ -0,0 +1,8 @@
+package com.dio.personalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveApplication {
+}
